d755d2a40d8b0bd1e1394dc638c14dbc421a3df2
Jairo Meléndez <jairo.melendez@urp.edu.pe>
2024-05-20 18:52:05 -0500

    Show gallery with javascript

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@
 //Llamando a la función
 // exports.task = task;
 
-
 // function nombreDeLaFuncion(parametro1, parametro2, ...) {
 // Contenido de la función
 // }
@@ -26,46 +25,44 @@
 //Una variable global
 //var a = 5;
 
-
 //Se quiere utilizar y automatizar las funciones de la librería sass
 //Se importa la función src y dest de gulp
-const { src, dest, watch, parallel } = require('gulp')
+const { src, dest, watch, parallel } = require("gulp");
 // CSS
 //Se define una constante que capture la automatización de gusp para sass y a su vez se indica sass como parámetro
-const sass = require('gulp-sass')(require('sass'))
+const sass = require("gulp-sass")(require("sass"));
 //Utilizando la función plumber de gulp, se importa la libería gulp-plumber a través de esta instancia
-const plumber = require('gulp-plumber')
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
-const postcss = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
+const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 
 // Javascript
 
-const terser = require('gulp-terser-js')
+const terser = require("gulp-terser-js");
 
 // Imágenes
 //Importación de gulp-cache
-const cache = require('gulp-cache')
+const cache = require("gulp-cache");
 //Importación de gulp-imagein
-const imagemin = require('gulp-imagemin')
+const imagemin = require("gulp-imagemin");
 //Importación de gulp-webp
-const webp = require('gulp-webp')
+const webp = require("gulp-webp");
 //Importación de gulp-avif
-const avif = require('gulp-avif')
+const avif = require("gulp-avif");
 
 function css(done) {
-  src('src/scss/**/*.scss') // Identificar el archivo SASS
+  src("src/scss/**/*.scss") // Identificar el archivo SASS
     .pipe(sourcemaps.init())
     //pipe se utiliza para ejecutar una función después de otro
     //En caso de que hayan problemas no se cerrará abruptamente la ejecución, no se detiene el workflow
     .pipe(plumber()) // Si tiene problemas, no detiene el workflow
     .pipe(sass()) // Compilarlo
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write("."))
     //Se guarda el nuevo código en una nueva carpeta, en el disco duro
-    .pipe(dest('build/css'));
-
+    .pipe(dest("build/css"));
 
   done(); // Callback que avisa a gulp cuando llegamos al final
 }
@@ -75,14 +72,14 @@ function images(done) {
   //Creación de un objeto
   const options = {
     //Atributo que optimiza 3 veces las imágenes
-    optimizationLevel: 3
-  }
+    optimizationLevel: 3,
+  };
   //Que parte de la carpeta img y que agarre los archivos .jpg, png
-  src('src/img/**/*.{jpg,png}')
+  src("src/img/**/*.{jpg,png}")
     //Que los guarde en cache (Lugar de acceso rápido), se optimiza con options
     .pipe(cache(imagemin(options)))
     //Se pasa a la carpeta de destino, que se creará al ejecutar la función
-    .pipe(dest('build/img'))
+    .pipe(dest("build/img"));
 
   //Callback para indicar que la función acabó
   done();
@@ -92,16 +89,16 @@ function images(done) {
 function versionWebp(done) {
   const options = {
     //Calidad de 50
-    quality: 50
-  }
+    quality: 50,
+  };
 
   //Sacar las imágenes dentro de la carpeta de images en src, los lee desde esta carpeta
-  src('src/img/**/*.{jpg,png}')
+  src("src/img/**/*.{jpg,png}")
     //Conversión en webp siguiendo las indicaciones de option
     .pipe(webp(options))
     //Guardándolo en el destino indicado
-    .pipe(dest('build/img'))
-  
+    .pipe(dest("build/img"));
+
   done();
 }
 
@@ -109,24 +106,26 @@ function versionWebp(done) {
 function versionAvif(done) {
   const options = {
     //Calidad de 50
-    quality: 50
-  }
+    quality: 50,
+  };
 
   //Sacar las imágenes dentro de la carpeta de images en src, los lee desde esta carpeta
-  src('src/img/**/*.{jpg,png}')
+  src("src/img/**/*.{jpg,png}")
     //Conversión en webp siguiendo las indicaciones de option usando la librería avif
     .pipe(avif(options))
     //Guardándolo en el destino indicado
-    .pipe(dest('build/img'))
+    .pipe(dest("build/img"));
   done();
 }
 
+//Función que nos permite escuchar archivos JavaScript
 function javascript(done) {
-  src('src/js/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(terser())
-    .pipe(sourcemaps.write('.'))
-    .pipe(dest('build/js'));
+  //Ruta de fuente de nuestros archivos JavaScript
+  src("src/js/**/*.js")
+    // .pipe(sourcemaps.init())
+    // .pipe(terser())
+    // .pipe(sourcemaps.write("."))
+    .pipe(dest("build/js"));
 
   done();
 }
@@ -134,18 +133,18 @@ function javascript(done) {
 //Función que indica que se encuentra en modo de desarrollo
 function dev(done) {
   //Ejecutando la función css cuando se detectan cambios en archivos sass dentro de la carpeta src\scss
-  watch('src/scss/**/*.scss', css);
-  watch('src/js/**/*.js', javascript);
+  watch("src/scss/**/*.scss", css);
+  //Que mire o detecto los cambios en la carpeta y se llame a la función javascript
+  watch("src/js/**/*.js", javascript);
 
   //Además, se le indica cuando la función va acabar
   done();
 }
 
-
 function prod(done) {
-  src('src/scss/**/*.scss') // Identificar el archivo SASS
+  src("src/scss/**/*.scss") // Identificar el archivo SASS
     .pipe(sass()) // Compilarlo
-    .pipe(dest('build/css')); // Almacenarla en el disco duro
+    .pipe(dest("build/css")); // Almacenarla en el disco duro
 
   done();
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,90 +1,86 @@
-document.addEventListener('DOMContentLoaded', () => { 
-    initApp();
+//Se le agrega un evento al HTML que espere a que el contenido del documento se cargue, se inicia el javascript
+document.addEventListener("DOMContentLoaded", () => {
+  //Que inicie la aplicación
+  initApp();
 });
 
 function initApp() {
-  fixedNavigation();
   createGallery();
-  scrollNav();
 }
 
-function fixedNavigation() {
-  const bar = document.querySelector('.header');
-  const aboutFestival = document.querySelector('.about-festival');
-  const body = document.querySelector('body');
-  window.addEventListener('scroll', () => {
-    if ( aboutFestival.getBoundingClientRect().bottom < 0 ) {
-      bar.classList.add('fixed');
-      body.classList.add('body-scroll');
-    } else {
-      bar.classList.remove('fixed');
-      body.classList.remove('body-scroll');
-    }
-  });
-}
-
-function scrollNav() {
-  const links = document.querySelectorAll('.navigation-main a');
-  links.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      const sectionScroll = e.target.attributes.href.value;
-      const section = document.querySelector(sectionScroll);
-      section.scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  })
-}
-
+//Función que crea la galería
 function createGallery() {
-  const gallery = document.querySelector('.gallery-images');
-  
+  //En un constante se selecciona la galería del html
+  const gallery = document.querySelector(".gallery-images");
+  //Bucle mediante el cual se realizará la creación de 12 imágenes
   for (let i = 1; i <= 12; i++) {
-    const image = document.createElement('picture')
+    //Se crea una constante image que tenga como valor el tag de picture de HTML
+    const image = document.createElement("picture");
+    //innerHTML es una propiedad que nos permite agregar código HTML dentro de nuestro elemento, en este caso dentro del tag image
+    //Dentro de este tag se agregará las imágenes minificadas de acuerdo a su calidad, por cada unas imágenes minificadas
     image.innerHTML = `
-      <source srcset="build/img/thumb/${i}.avif" type="image/avif">
-      <source srcset="build/img/thumb/${i}.webp" type="image/webp">
-      <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galería ${i}">
+                <source srcset="build/img/thumb/${i}.avif" type="image/avif">
+                <source srcset="build/img/thumb/${i}" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg"
+                    alt="Imagen Galería ${i}">
     `;
+    //Se agrega al elemento image el evento onclick, que tiene un callback a la función showimage, es decir que cuando se realice un click en la imagen
     image.onclick = () => {
-      showImage(i)
-    }
+      showImage(i);
+    };
+    //Se agrega a gallery-images el elemnto image, uno por uno, de acuerdo al bucle
     gallery.appendChild(image);
   }
-
 }
 
 function showImage(id) {
-  const image = document.createElement('picture');
+  //Se crea un elemento HTML picture
+  const image = document.createElement("picture");
+  //Dentro del elemento se pone las imágenes en alta definición, las originales
   image.innerHTML = `
-      <source srcset="build/img/grande/${id}.avif" type="image/avif">
-      <source srcset="build/img/grande/${id}.webp" type="image/webp">
-      <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galería ${id}">
+  <source srcset="build/img/grande/${id}.avif" type="image/avif">
+                <source srcset="build/img/grande/${id}" type="image/webp">
+                <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg"
+                    alt="Imagen Galería ${id}">
   `;
-  // Crea el Overlay con la imagen
-  const overlay = document.createElement('div');
-  overlay.appendChild(image);
-  overlay.classList.add('overlay');
-  overlay.onclick = () => {
-    const body = document.querySelector('body');
-    body.classList.remove('no-scroll');
-    overlay.remove();
-  }
 
-  // Botón para cerrar el Modal
-  const closeModal = document.createElement('p');
-  closeModal.textContent = 'X';
-  closeModal.classList.add('btn-close');
-  closeModal.onclick = () => {
-    const body = document.querySelector('body');
-    body.classList.remove('no-scroll');
+  //Se crea un overlay, es decir un contenedor, un div
+  const overlay = document.createElement("div");
+  //Se agrega la imagen al contenedor
+  overlay.appendChild(image);
+  //Se le agrega una clase llamada overlay para brindarle estilos
+  overlay.classList.add("overlay");
+  //Que cuando seleccione cualquier parte del overlay, también cierre la imagen
+  overlay.onclick = () => {
+    //Se selecciona el body
+    const body = document.querySelector("body");
+    //Quitarle el no-scroll
+    body.classList.remove("no-scroll");
+    //Quitar el overlay
     overlay.remove();
-  }
+  };
+
+  //Botón para cerrar la imagen
+  //Se crea un elemento de tipo de párrafo
+  const closeModal = document.createElement("p");
+  //Que tenga como texto de contenido una X
+  closeModal.textContent = "X";
+  //Se agrega al elemento una clase
+  closeModal.classList.add("btn-close");
+  //Cuando se realice un click en este elemento, se elimine el overlay
+  closeModal.onclick = () => {
+    const body = document.querySelector("body");
+    //Se desactiva el no-scroll
+    body.classList.remove("no-scroll");
+    overlay.remove();
+  };
+  //Se agrega al overlay el closeModal
   overlay.appendChild(closeModal);
 
-  // Añade el Overlay al HTML
-  const body = document.querySelector('body');
+  //Se selecciona el body del HTML
+  const body = document.querySelector("body");
+  //Se agrega el overlay finalmente el contenedor al body
   body.appendChild(overlay);
-  body.classList.add('no-scroll')
+  //Cuando se ejecute la función se activa el no-scroll
+  body.classList.add("no-scroll");
 }
